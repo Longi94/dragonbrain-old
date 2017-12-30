db1f3808e1e8bcd7237fa8a391399fb85c3e9e4f
Long Tran <lngtrn94@gmail.com>
2017-12-30 12:27:52 +0100

    fixed multiple admin calls

--- dragonbrain/src/main/resources/static/js/admin.js
+++ dragonbrain/src/main/resources/static/js/admin.js
@@ -13,6 +13,18 @@ $('document').ready(function () {
     newProjectDialog = new mdc.dialog.MDCDialog(document.querySelector('#dialog-add-project'));
     newPhotoDialog = new mdc.dialog.MDCDialog(document.querySelector('#dialog-add-photo'));
 
+    newProjectDialog.listen('MDCDialog:cancel', function () {
+        $("#dialog-add-project").find("input,textarea").val("");
+        newProjectDialog.unlisten('MDCDialog:accept', newProjectAccept);
+        newProjectDialog.unlisten('MDCDialog:accept', editProjectAccept);
+    });
+
+    newPhotoDialog.listen('MDCDialog:cancel', function () {
+        $("#dialog-add-photo").find("input").val("");
+        newPhotoDialog.unlisten('MDCDialog:accept', newPhotoAccept);
+        newPhotoDialog.unlisten('MDCDialog:accept', editPhotoAccept);
+    });
+
     $("#list-photo.mdc-list").find(".mdc-list-item").click(function () {
         projectMenu.open = false;
         photoMenu.open = false;
@@ -87,86 +99,84 @@ function openNewProjectDialog(event) {
     newProjectDialog.show();
     closeFab();
 
-    newProjectDialog.listen('MDCDialog:accept', function () {
-        var params = {
-            name: $("#new-project-name").val(),
-            type: $("#new-project-type").val(),
-            description: $("#new-project-description").val(),
-            sourceUrl: $("#new-project-source").val(),
-            url: $("#new-project-link").val(),
-            image: $("#new-project-image").val()
-        };
-
-        authAjax({
-            url: "/admin/projects",
-            type: "POST",
-            data: JSON.stringify(params),
-            contentType: "application/json",
-            dataType: "json",
-            success: function (data) {
-                var $listProject = $("#list-project");
-
-                $listProject.append('<li role="separator" class="mdc-list-divider"></li>')
-                    .append(
-                        '<li class="mdc-list-item" value="' + data.id + '">' +
-                        '<span class="mdc-list-item__text">' + data.name +
-                        '<span class="mdc-list-item__secondary-text">' + data.type + '</span>' +
-                        '</span>' +
-                        '<i class="mdc-list-item__end-detail material-icons" onclick="openProjectMenu(event, this);">more_vert</i>' +
-                        '</li>'
-                    );
-            }
-        });
-
-        $("#dialog-add-project").find("input,textarea").val("");
-    });
-
-    newProjectDialog.listen('MDCDialog:cancel', function () {
-        $("#dialog-add-project").find("input,textarea").val("");
-    });
+    newProjectDialog.listen('MDCDialog:accept', newProjectAccept);
 }
+
+var newProjectAccept = function () {
+    var params = {
+        name: $("#new-project-name").val(),
+        type: $("#new-project-type").val(),
+        description: $("#new-project-description").val(),
+        sourceUrl: $("#new-project-source").val(),
+        url: $("#new-project-link").val(),
+        image: $("#new-project-image").val()
+    };
+
+    authAjax({
+        url: "/admin/projects",
+        type: "POST",
+        data: JSON.stringify(params),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+            var $listProject = $("#list-project");
+
+            $listProject.append('<li role="separator" class="mdc-list-divider"></li>')
+                .append(
+                    '<li class="mdc-list-item" value="' + data.id + '">' +
+                    '<span class="mdc-list-item__text">' + data.name +
+                    '<span class="mdc-list-item__secondary-text">' + data.type + '</span>' +
+                    '</span>' +
+                    '<i class="mdc-list-item__end-detail material-icons" onclick="openProjectMenu(event, this);">more_vert</i>' +
+                    '</li>'
+                );
+        }
+    });
+
+    $("#dialog-add-project").find("input,textarea").val("");
+    newProjectDialog.unlisten('MDCDialog:accept', newProjectAccept);
+};
 
 function openNewPhotoDialog(event) {
     newPhotoDialog.lastFocusedTarget = event.target;
     newPhotoDialog.show();
     closeFab();
 
-    newPhotoDialog.listen('MDCDialog:accept', function () {
-        var params = {
-            title: $("#new-photo-title").val(),
-            device: $("#new-photo-device").val(),
-            date: $("#new-photo-date").val(),
-            path: $("#new-photo-image").val()
-        };
-
-        authAjax({
-            url: "/admin/photos",
-            type: "POST",
-            data: JSON.stringify(params),
-            contentType: "application/json",
-            dataType: "json",
-            success: function (data) {
-                var $listPhoto = $("#list-photo");
-
-                $listPhoto.append('<li role="separator" class="mdc-list-divider"></li>')
-                    .append(
-                        '<li class="mdc-list-item" value="' + data.id + '">' +
-                        '<span class="mdc-list-item__text">' + data.title +
-                        '<span class="mdc-list-item__secondary-text">' + data.device + '</span>' +
-                        '</span>' +
-                        '<i class="mdc-list-item__end-detail material-icons" onclick="openPhotoMenu(event, this);">more_vert</i>' +
-                        '</li>'
-                    );
-            }
-        });
-
-        $("#dialog-add-photo").find("input").val("");
-    });
-
-    newPhotoDialog.listen('MDCDialog:cancel', function () {
-        $("#dialog-add-photo").find("input").val("");
-    });
+    newPhotoDialog.listen('MDCDialog:accept', newPhotoAccept);
 }
+
+var newPhotoAccept = function () {
+    var params = {
+        title: $("#new-photo-title").val(),
+        device: $("#new-photo-device").val(),
+        date: $("#new-photo-date").val(),
+        path: $("#new-photo-image").val()
+    };
+
+    authAjax({
+        url: "/admin/photos",
+        type: "POST",
+        data: JSON.stringify(params),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (data) {
+            var $listPhoto = $("#list-photo");
+
+            $listPhoto.append('<li role="separator" class="mdc-list-divider"></li>')
+                .append(
+                    '<li class="mdc-list-item" value="' + data.id + '">' +
+                    '<span class="mdc-list-item__text">' + data.title +
+                    '<span class="mdc-list-item__secondary-text">' + data.device + '</span>' +
+                    '</span>' +
+                    '<i class="mdc-list-item__end-detail material-icons" onclick="openPhotoMenu(event, this);">more_vert</i>' +
+                    '</li>'
+                );
+        }
+    });
+
+    $("#dialog-add-photo").find("input").val("");
+    newPhotoDialog.unlisten('MDCDialog:accept', newPhotoAccept);
+};
 
 function deleteSelectedProject() {
     authAjax({
@@ -237,42 +247,42 @@ function openEditProjectDialog(event) {
             $("#new-project-link").val(project.url);
             $("#new-project-image").val(project.image);
 
+            newProjectDialog.root_.projectId = project.id;
             newProjectDialog.lastFocusedTarget = event.target;
             newProjectDialog.show();
 
-            newProjectDialog.listen('MDCDialog:accept', function () {
-                var params = {
-                    name: $("#new-project-name").val(),
-                    type: $("#new-project-type").val(),
-                    description: $("#new-project-description").val(),
-                    sourceUrl: $("#new-project-source").val(),
-                    url: $("#new-project-link").val(),
-                    image: $("#new-project-image").val()
-                };
-
-                authAjax({
-                    url: "/admin/projects/" + project.id,
-                    type: "PUT",
-                    data: JSON.stringify(params),
-                    contentType: "application/json",
-                    dataType: "json",
-                    success: function (project) {
-                        var $toEdit = $("#list-project").find(".mdc-list-item[value='" + selectedProject + "']");
-
-                        $toEdit.find(".mdc-list-item__text").text(project.name)
-                            .append('<span class="mdc-list-item__secondary-text">' + project.type + '</span>')
-                    }
-                });
-
-                $("#dialog-add-project").find("input,textarea").val("");
-            });
-
-            newProjectDialog.listen('MDCDialog:cancel', function () {
-                $("#dialog-add-project").find("input,textarea").val("");
-            });
+            newProjectDialog.listen('MDCDialog:accept', editProjectAccept);
         }
     });
 }
+
+var editProjectAccept = function (evt) {
+    var params = {
+        name: $("#new-project-name").val(),
+        type: $("#new-project-type").val(),
+        description: $("#new-project-description").val(),
+        sourceUrl: $("#new-project-source").val(),
+        url: $("#new-project-link").val(),
+        image: $("#new-project-image").val()
+    };
+
+    authAjax({
+        url: "/admin/projects/" + evt.target.projectId,
+        type: "PUT",
+        data: JSON.stringify(params),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (project) {
+            var $toEdit = $("#list-project").find(".mdc-list-item[value='" + selectedProject + "']");
+
+            $toEdit.find(".mdc-list-item__text").text(project.name)
+                .append('<span class="mdc-list-item__secondary-text">' + project.type + '</span>')
+        }
+    });
+
+    $("#dialog-add-project").find("input,textarea").val("");
+    newProjectDialog.unlisten('MDCDialog:accept', editProjectAccept);
+};
 
 function openEditPhotoDialog(event) {
 
@@ -285,37 +295,37 @@ function openEditPhotoDialog(event) {
             $("#new-photo-date").val(photo.date);
             $("#new-photo-image").val(photo.path);
 
+            newPhotoDialog.root_.photoId = photo.id;
             newPhotoDialog.lastFocusedTarget = event.target;
             newPhotoDialog.show();
 
-            newPhotoDialog.listen('MDCDialog:accept', function () {
-                var params = {
-                    title: $("#new-photo-title").val(),
-                    device: $("#new-photo-device").val(),
-                    date: $("#new-photo-date").val(),
-                    path: $("#new-photo-image").val()
-                };
-
-                authAjax({
-                    url: "/admin/photos/" + photo.id,
-                    type: "PUT",
-                    data: JSON.stringify(params),
-                    contentType: "application/json",
-                    dataType: "json",
-                    success: function (photo) {
-                        var $toEdit = $("#list-photo").find(".mdc-list-item[value='" + selectedPhoto + "']");
-
-                        $toEdit.find(".mdc-list-item__text").text(photo.title)
-                            .append('<span class="mdc-list-item__secondary-text">' + photo.device + '</span>')
-                    }
-                });
-
-                $("#dialog-add-photo").find("input").val("");
-            });
-
-            newPhotoDialog.listen('MDCDialog:cancel', function () {
-                $("#dialog-add-photo").find("input").val("");
-            });
+            newPhotoDialog.listen('MDCDialog:accept', editPhotoAccept);
         }
     });
 }
+
+var editPhotoAccept = function (evt) {
+    var params = {
+        title: $("#new-photo-title").val(),
+        device: $("#new-photo-device").val(),
+        date: $("#new-photo-date").val(),
+        path: $("#new-photo-image").val()
+    };
+
+    authAjax({
+        url: "/admin/photos/" + evt.target.photoId,
+        type: "PUT",
+        data: JSON.stringify(params),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (photo) {
+            var $toEdit = $("#list-photo").find(".mdc-list-item[value='" + selectedPhoto + "']");
+
+            $toEdit.find(".mdc-list-item__text").text(photo.title)
+                .append('<span class="mdc-list-item__secondary-text">' + photo.device + '</span>')
+        }
+    });
+
+    $("#dialog-add-photo").find("input").val("");
+    newPhotoDialog.unlisten('MDCDialog:accept', editPhotoAccept);
+};
